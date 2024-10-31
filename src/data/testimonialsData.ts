a01c3c50d6d6b91f7a1375d0dc2ddd392f22cb1b
mgGabrielly <ggabrielly.ggaby@gmail.com>
2024-10-30 22:32:16 -0300

    Adding testimonial

--- src/data/testimonialsData.ts
+++ src/data/testimonialsData.ts
@@ -1,8 +1,8 @@
 export const testimonials = [
   {
-    text: 'O Reforço Passo a Passo transformou a experiência escolar do meu filho! Ele está se sentindo mais confiante e motivado a aprender.',
-    author: 'Fulana',
-    relationship: 'Mãe de Fulano, 8 anos',
+    text: 'Quero dedicar esse tempinho para reforçar a importância do acompanhamento no reforço Passo a Passo, identificamos que apenas a didática escola estava insuficiente para os resultados desejados, então iniciou a luta para encontrar um espaço confiável, profissional e cuidadoso, encontramos tia Wylma, profissional excelente, amorosa, preocupada e empenhada com os resultados do meu pequeno, não só ela, mais toda sua equipe, hj vejo meu pequeno lendo, calculando perfeitamente e ama o espaço, as festividades, os coleguinhas, enfim, encerro agradecendo todo apoio e companheirismo durante esses anos, e já aviso que está chegando mais um integrante para galgar grandes resultados',
+    author: 'Paula Holanda',
+    relationship: 'Mãe de João Lucas, 3º ano',
   },
   {
     text: 'As atividades são divertidas e o apoio dos professores é incrível!',
